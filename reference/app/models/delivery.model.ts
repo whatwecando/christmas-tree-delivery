f54f1b1c9b87b6aee74199eaf815691b75973c4e
whatwecando <whatwecando@gmail.com>
2024-12-02 17:59:19 +0100

    chore: remove obsolete reference folder

--- reference/app/models/delivery.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Delivery {
-    id: string;
-    customerName: string;
-    address: string;
-    phoneNumber: string;
-    treeSize: string;
-    isDelivered: boolean;
-    deliveryDate?: Date;
-}
