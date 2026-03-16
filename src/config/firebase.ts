de8373a616cb1c69c243b989dc15936b2abd5e77
Philippe Caron <pcarontelders@gmail.com>
2026-03-16 16:01:20 +0100

    security: move Firebase config to env variables (VITE_FIREBASE_*)

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY_HERE",
-  authDomain: "sapin-de-saint-roch.firebaseapp.com",
-  databaseURL: "https://sapin-de-saint-roch-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "sapin-de-saint-roch",
-  storageBucket: "sapin-de-saint-roch.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "1:YOUR_MESSAGING_SENDER_ID:web:YOUR_APP_ID_SUFFIX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
